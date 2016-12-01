2f2f6dd9972784b96e7e99c2ee7716c5c7c333ec
velina.kozareva@gmail.com <velina.kozareva@gmail.com>
2016-12-01 00:26:48 -0500

    testing new requirements

--- working.py
+++ working.py
@@ -1,11 +1,12 @@
 '''Receive messages through twilio API and send to gmail'''
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
 import twilio.twiml
 from twilio.rest import TwilioRestClient
+from datetime import datetime, timedelta
 # import sendgrid
 # import os
-#from sendgrid.helpers.mail import *
+# from sendgrid.helpers.mail import *
 # import smtplib
 # from email.mime.text import MIMEText
 
@@ -16,20 +17,22 @@ from_number = "+19092199424"
 lynn_number = "+19095291698"
 velina_number = '+18036736204'
 velina_email = 'velina.kozareva@gmail.com'
-
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+days_seg = []
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 #numbers = []
 #FIREBASE_URL = "https://pronto-health.firebaseio.com/"
 #fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
 
-# """
-#     user_update = {
-#         "last_msg_type": "Intro",
-#         "nudge_type" : 
-#     }
-#     fb.patch('/user/' + sender + '/', user_update)
-#     user_details = fb.get('/user', sender)
+"""
+    user_update = {
+        "last_msg_type": "Intro",
+        "nudge_type" : 
+    }
+    fb.patch('/user/' + sender + '/', user_update)
+    user_details = fb.get('/user', sender)
  
-# """
+"""
 # @app.route("/")
 # def index():
 #   return "Hello World"
@@ -71,15 +74,34 @@ def receieve_sms():
     forward = "Response from {}: {}".format(from_number, body)
     to_number = from_number 
 
+    time = None
+    if body.find('pm') > -1 or body.find('am') > -1:
+        if body.find(':') > -1:
+            # +6 to account for space between time and 'pm'/'am'
+            time = {
+                'text': body[body.find(':')-1:body.find(':')+6],
+                'hour': int(body[body.find(':')-1])
+            }
+        else:
+            time_pos = body.find('pm') or body.find('am')
+            time = {
+                'text': body[time_pos-1:time_pos+3],
+                'hour': int(body[time_pos-1])
+            }
+    if time:
+        if body.find('before') > -1:
+
+            forward = 'Ok, you are scheduled for {}'
+
     if body.lower() == 'yes':
         forward = 'Please text back days next week (Monday-Friday) during which you would be able to schedule an appointment, "where" if you would like to know where the clinic is located, or "more info" if you have questions about insurance or other details.'
-    elif 'where' in body.lower().split(' '):
+    elif body.lower().find('where') >-1:
         forward = 'We are located at 13768 Roswell Ave. in Chino off the 71. Please text back days (Monday-Friday) during which you would be able to schedule an appointment, or "more info" if you have further questions.'
     else:
         forward = "Response from {}: {}".format(from_number, body)
         to_number = velina_number
 
-    send_sms(to_number, forward)
+    #send_sms(to_number, forward)
     #send_email(velina_email, from_number, forward)
     return "OK"
 
