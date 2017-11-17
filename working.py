58e6c050053cfe81c385e8e1e7144d3c77b2054f
Lynn Wang <lynncwang@gmail.com>
2017-11-17 11:42:44 -0800

    turned bot off

--- working.py
+++ working.py
@@ -136,13 +136,11 @@ def receieve_sms():
    
     to_number = lynn_number 
 
-    """
     #comment this area out to turn chatbot on
     update_log = '{} --{} --{}'.format(message_log, body, 'AWAITING RESPONSE')
     send_sms(to_number, update_log, from_number)
     return 'OK'
     #end of chatbot comment area#
-    """
 
     #update_appt = '{} {}'.format(appt_set, '')
 
